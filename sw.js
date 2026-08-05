1910d8623fea63e7a331ed39f9a29d59310f199e
jesscorrao-droid <jess.corrao@gmail.com>
2026-08-05 03:09:40 +0200

    Rewrite service worker

--- sw.js
+++ sw.js
@@ -1,76 +1,81 @@
-const CACHE = "weddba-v1.0.4";
+const CACHE_NAME = "weddba-v2.0.0";
 
-const FILES = [
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.json",
 
-"./",
-"./index.html",
-"./style.css",
-"./script.js",
-"./data.js",
-"./manifest.json",
+    "./script.js",
+    "./data.js",
+    "./install.js",
 
-"./photos.html",
-"./photos.css",
-"./photos.js",
+    "./css/style.css",
+    "./css/header.css",
+    "./css/hero.css",
+    "./css/cards.css",
+    "./css/footer.css",
+    "./css/pwa.css",
+    "./css/responsive.css",
 
-"./picture.html",
-"./picture.css",
-"./picture.js",
-
-"./halftime.html",
-"./halftime.css",
-
-"./celibato.html",
-"./celibato.js",
-
-"./nubilato.html",
-"./nubilato.js",
-
-"./player.html"
-
+    "./images/logo.png",
+    "./images/icon-192.png",
+    "./images/icon-512.png",
+    "./images/hero.png",
+    "./images/hero-mobile.png"
 ];
 
-self.addEventListener("install",event=>{
-
+// INSTALL
+self.addEventListener("install", event => {
     self.skipWaiting();
 
     event.waitUntil(
-
-        caches.open(CACHE).then(cache=>cache.addAll(FILES))
-
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
     );
-
 });
 
-self.addEventListener("activate",event=>{
+// ACTIVATE
+self.addEventListener("activate", event => {
 
     event.waitUntil(
+        caches.keys().then(keys =>
 
-        caches.keys().then(keys=>{
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
 
-            return Promise.all(
-
-                keys.filter(key=>key!==CACHE)
-
-                .map(key=>caches.delete(key))
-
-            );
-
-        })
-
+        ).then(() => self.clients.claim())
     );
-
-    self.clients.claim();
 
 });
 
-self.addEventListener("fetch",event=>{
+// FETCH
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") return;
 
     event.respondWith(
 
         caches.match(event.request)
+            .then(cached => {
 
-        .then(response=>response || fetch(event.request))
+                if (cached) return cached;
+
+                return fetch(event.request)
+                    .then(response => {
+
+                        const copy = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => cache.put(event.request, copy));
+
+                        return response;
+
+                    });
+
+            })
 
     );
 
